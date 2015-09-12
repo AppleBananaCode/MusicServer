ba2d64ff3326fec3f3bdf41487eed34cbbdff555
maweiyi <ma823285807@gmail.com>
2015-09-12 12:33:31 +0800

    Second commit

--- music/urls.py
+++ music/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import include, url
 from .views import test
-from .views import test2
+from .views import getTop_playlistsId
+from .views import test3
 
 urlpatterns = [
-    url(r'^hello/',test),
-    url(r'hello1/',test2)
+    url(r'^hello/', test),
+    url(r'^hello1/', getTop_playlistsId),
+    url(r'^hello2/', test3)
 
 ]
--- music/views.py
+++ music/views.py
@@ -1,18 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from django.http import HttpResponse
 from django.shortcuts import render
 from api import NetEase
 import simplejson
 # Create your views here.
 
+getLists = []
+# 获取top歌单的详细内容
 def test(request):
     ease = NetEase()
-    s = ease.playlist_detail('105415765')
+    s = ease.playlist_detail('95537632')
 
 
     return HttpResponse(s)
 
-
-def test2(request):
+# 获取top歌单Id
+def getTop_playlistsId(request):
+    getlistId = []
+    getDict = {}
+    #getDict.
     ease = NetEase()
-    s = ease.top_playlists(limit=1)
-    return HttpResponse(s)
+    s = ease.top_playlists(limit=50)
+    req = simplejson.dumps(s)
+    getReq = simplejson.loads(req)
+    print(getReq)
+   # req = simplejson.loads(s)
+    # 遍历整个字典取出需要的数据并加入到list
+    for getid in getReq:
+        global getLists
+        getLists.append(getid["id"])
+
+    return HttpResponse(getLists)
+
+
+def test3(request):
+    ease = NetEase()
+    s = ease.song_detail('30590170')
+    print(getLists)
+    global getLists
+    return HttpResponse(getLists)
